a2405381a0df396b41c9fd19ee4998ec307c785a
OnBran <156179679+OnBran@users.noreply.github.com>
2024-03-06 08:40:36 +0100

    Add files via upload

---
+++ Kalkulačka/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Kalkulačka
+{
+    partial class Kalkulačka
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplej = new System.Windows.Forms.TextBox();
+            this.btCislo1 = new System.Windows.Forms.Button();
+            this.btCislo2 = new System.Windows.Forms.Button();
+            this.btCislo3 = new System.Windows.Forms.Button();
+            this.btCislo4 = new System.Windows.Forms.Button();
+            this.btCislo5 = new System.Windows.Forms.Button();
+            this.btCislo6 = new System.Windows.Forms.Button();
+            this.btCislo7 = new System.Windows.Forms.Button();
+            this.btCislo8 = new System.Windows.Forms.Button();
+            this.btCislo9 = new System.Windows.Forms.Button();
+            this.btCislo0 = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btOperacePlus = new System.Windows.Forms.Button();
+            this.btOperaceMinus = new System.Windows.Forms.Button();
+            this.btOperaceKrat = new System.Windows.Forms.Button();
+            this.btOperaceDeleno = new System.Windows.Forms.Button();
+            this.btOperaceRovnase = new System.Windows.Forms.Button();
+            this.btBackspace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplej
+            // 
+            this.txtDisplej.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplej.Location = new System.Drawing.Point(12, 22);
+            this.txtDisplej.Name = "txtDisplej";
+            this.txtDisplej.ReadOnly = true;
+            this.txtDisplej.Size = new System.Drawing.Size(336, 40);
+            this.txtDisplej.TabIndex = 0;
+            this.txtDisplej.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btCislo1
+            // 
+            this.btCislo1.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo1.Location = new System.Drawing.Point(12, 209);
+            this.btCislo1.Name = "btCislo1";
+            this.btCislo1.Size = new System.Drawing.Size(75, 59);
+            this.btCislo1.TabIndex = 1;
+            this.btCislo1.Text = "1";
+            this.btCislo1.UseVisualStyleBackColor = true;
+            this.btCislo1.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo2
+            // 
+            this.btCislo2.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo2.Location = new System.Drawing.Point(93, 209);
+            this.btCislo2.Name = "btCislo2";
+            this.btCislo2.Size = new System.Drawing.Size(75, 59);
+            this.btCislo2.TabIndex = 2;
+            this.btCislo2.Text = "2";
+            this.btCislo2.UseVisualStyleBackColor = true;
+            this.btCislo2.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo3
+            // 
+            this.btCislo3.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo3.Location = new System.Drawing.Point(174, 209);
+            this.btCislo3.Name = "btCislo3";
+            this.btCislo3.Size = new System.Drawing.Size(75, 59);
+            this.btCislo3.TabIndex = 3;
+            this.btCislo3.Text = "3";
+            this.btCislo3.UseVisualStyleBackColor = true;
+            this.btCislo3.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo4
+            // 
+            this.btCislo4.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo4.Location = new System.Drawing.Point(12, 144);
+            this.btCislo4.Name = "btCislo4";
+            this.btCislo4.Size = new System.Drawing.Size(75, 59);
+            this.btCislo4.TabIndex = 4;
+            this.btCislo4.Text = "4";
+            this.btCislo4.UseVisualStyleBackColor = true;
+            this.btCislo4.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo5
+            // 
+            this.btCislo5.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo5.Location = new System.Drawing.Point(93, 144);
+            this.btCislo5.Name = "btCislo5";
+            this.btCislo5.Size = new System.Drawing.Size(75, 59);
+            this.btCislo5.TabIndex = 5;
+            this.btCislo5.Text = "5";
+            this.btCislo5.UseVisualStyleBackColor = true;
+            this.btCislo5.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo6
+            // 
+            this.btCislo6.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo6.Location = new System.Drawing.Point(174, 144);
+            this.btCislo6.Name = "btCislo6";
+            this.btCislo6.Size = new System.Drawing.Size(75, 59);
+            this.btCislo6.TabIndex = 6;
+            this.btCislo6.Text = "6";
+            this.btCislo6.UseVisualStyleBackColor = true;
+            this.btCislo6.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo7
+            // 
+            this.btCislo7.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo7.Location = new System.Drawing.Point(12, 79);
+            this.btCislo7.Name = "btCislo7";
+            this.btCislo7.Size = new System.Drawing.Size(75, 59);
+            this.btCislo7.TabIndex = 7;
+            this.btCislo7.Text = "7";
+            this.btCislo7.UseVisualStyleBackColor = true;
+            this.btCislo7.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo8
+            // 
+            this.btCislo8.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo8.Location = new System.Drawing.Point(93, 79);
+            this.btCislo8.Name = "btCislo8";
+            this.btCislo8.Size = new System.Drawing.Size(75, 59);
+            this.btCislo8.TabIndex = 8;
+            this.btCislo8.Text = "8";
+            this.btCislo8.UseVisualStyleBackColor = true;
+            this.btCislo8.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo9
+            // 
+            this.btCislo9.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo9.Location = new System.Drawing.Point(174, 79);
+            this.btCislo9.Name = "btCislo9";
+            this.btCislo9.Size = new System.Drawing.Size(75, 59);
+            this.btCislo9.TabIndex = 9;
+            this.btCislo9.Text = "9";
+            this.btCislo9.UseVisualStyleBackColor = true;
+            this.btCislo9.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo0
+            // 
+            this.btCislo0.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btCislo0.Location = new System.Drawing.Point(12, 274);
+            this.btCislo0.Name = "btCislo0";
+            this.btCislo0.Size = new System.Drawing.Size(75, 59);
+            this.btCislo0.TabIndex = 10;
+            this.btCislo0.Text = "0";
+            this.btCislo0.UseVisualStyleBackColor = true;
+            this.btCislo0.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btClear.Location = new System.Drawing.Point(174, 274);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(75, 59);
+            this.btClear.TabIndex = 11;
+            this.btClear.Text = "C";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperacePlus
+            // 
+            this.btOperacePlus.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btOperacePlus.Location = new System.Drawing.Point(273, 79);
+            this.btOperacePlus.Name = "btOperacePlus";
+            this.btOperacePlus.Size = new System.Drawing.Size(75, 59);
+            this.btOperacePlus.TabIndex = 12;
+            this.btOperacePlus.Text = "+";
+            this.btOperacePlus.UseVisualStyleBackColor = true;
+            this.btOperacePlus.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperaceMinus
+            // 
+            this.btOperaceMinus.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btOperaceMinus.Location = new System.Drawing.Point(273, 144);
+            this.btOperaceMinus.Name = "btOperaceMinus";
+            this.btOperaceMinus.Size = new System.Drawing.Size(75, 59);
+            this.btOperaceMinus.TabIndex = 13;
+            this.btOperaceMinus.Text = "-";
+            this.btOperaceMinus.UseVisualStyleBackColor = true;
+            this.btOperaceMinus.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperaceKrat
+            // 
+            this.btOperaceKrat.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btOperaceKrat.Location = new System.Drawing.Point(273, 209);
+            this.btOperaceKrat.Name = "btOperaceKrat";
+            this.btOperaceKrat.Size = new System.Drawing.Size(75, 59);
+            this.btOperaceKrat.TabIndex = 14;
+            this.btOperaceKrat.Text = "*";
+            this.btOperaceKrat.UseVisualStyleBackColor = true;
+            this.btOperaceKrat.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperaceDeleno
+            // 
+            this.btOperaceDeleno.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btOperaceDeleno.Location = new System.Drawing.Point(273, 274);
+            this.btOperaceDeleno.Name = "btOperaceDeleno";
+            this.btOperaceDeleno.Size = new System.Drawing.Size(75, 59);
+            this.btOperaceDeleno.TabIndex = 15;
+            this.btOperaceDeleno.Text = ":";
+            this.btOperaceDeleno.UseVisualStyleBackColor = true;
+            this.btOperaceDeleno.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperaceRovnase
+            // 
+            this.btOperaceRovnase.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btOperaceRovnase.Location = new System.Drawing.Point(93, 274);
+            this.btOperaceRovnase.Name = "btOperaceRovnase";
+            this.btOperaceRovnase.Size = new System.Drawing.Size(75, 59);
+            this.btOperaceRovnase.TabIndex = 16;
+            this.btOperaceRovnase.Text = "=";
+            this.btOperaceRovnase.UseVisualStyleBackColor = true;
+            this.btOperaceRovnase.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btBackspace
+            // 
+            this.btBackspace.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btBackspace.Location = new System.Drawing.Point(174, 339);
+            this.btBackspace.Name = "btBackspace";
+            this.btBackspace.Size = new System.Drawing.Size(75, 59);
+            this.btBackspace.TabIndex = 17;
+            this.btBackspace.Text = "⌫";
+            this.btBackspace.UseVisualStyleBackColor = true;
+            this.btBackspace.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // Kalkulačka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(367, 399);
+            this.Controls.Add(this.btBackspace);
+            this.Controls.Add(this.btOperaceRovnase);
+            this.Controls.Add(this.btOperaceDeleno);
+            this.Controls.Add(this.btOperaceKrat);
+            this.Controls.Add(this.btOperaceMinus);
+            this.Controls.Add(this.btOperacePlus);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btCislo0);
+            this.Controls.Add(this.btCislo9);
+            this.Controls.Add(this.btCislo8);
+            this.Controls.Add(this.btCislo7);
+            this.Controls.Add(this.btCislo6);
+            this.Controls.Add(this.btCislo5);
+            this.Controls.Add(this.btCislo4);
+            this.Controls.Add(this.btCislo3);
+            this.Controls.Add(this.btCislo2);
+            this.Controls.Add(this.btCislo1);
+            this.Controls.Add(this.txtDisplej);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Kalkulačka";
+            this.Text = "Kalkulačka";
+            this.Load += new System.EventHandler(this.Kalkulačka_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplej;
+        private System.Windows.Forms.Button btCislo1;
+        private System.Windows.Forms.Button btCislo2;
+        private System.Windows.Forms.Button btCislo3;
+        private System.Windows.Forms.Button btCislo4;
+        private System.Windows.Forms.Button btCislo5;
+        private System.Windows.Forms.Button btCislo6;
+        private System.Windows.Forms.Button btCislo7;
+        private System.Windows.Forms.Button btCislo8;
+        private System.Windows.Forms.Button btCislo9;
+        private System.Windows.Forms.Button btCislo0;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btOperacePlus;
+        private System.Windows.Forms.Button btOperaceMinus;
+        private System.Windows.Forms.Button btOperaceKrat;
+        private System.Windows.Forms.Button btOperaceDeleno;
+        private System.Windows.Forms.Button btOperaceRovnase;
+        private System.Windows.Forms.Button btBackspace;
+    }
+}
+
---
+++ Kalkulačka/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulačka
+{
+    public partial class Kalkulačka : Form
+    {
+       // provadena operace
+        enum enOperace {plus, minus, krat, deleno, clear, rovnase};
+
+        //stavové proměnné
+        float mflCislo1, mflCislo2, mflVysledek;
+        bool mblNovaOperace;
+        enOperace menAktOperace;
+        //---------------------------------------------------------------------------------------------------------------------------
+        // konstruktor
+        //---------------------------------------------------------------------------------------------------------------------------
+        public Kalkulačka()
+        {
+            InitializeComponent();
+
+            // init promennych
+            VseVymazat();
+            txtDisplej.Text = "0";
+        }
+        private void btCislo_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                // smazat úvodní nulu
+                if (mblNovaOperace == true) txtDisplej.Text = "";
+                mblNovaOperace = false;
+
+                // přidat další číslo
+                txtDisplej.Text = txtDisplej.Text + ((Button)sender).Text;
+            }
+            catch   (Exception ex)
+            {
+            }      
+        }
+        private void btOperace_Click(object sender, EventArgs e)
+        {
+            Button lobjTlacitko;
+
+            // zavola funkci
+            lobjTlacitko = (Button)sender;
+
+            switch (lobjTlacitko.Text)
+            {
+                // clear
+                case "C": VseVymazat();
+                    mblNovaOperace=true;
+                    txtDisplej.Text = "0";
+                    break;
+                // backspace
+                case "⌫": PosledniVymazat();
+                    mblNovaOperace = true;
+                    break;
+                // plus
+                case "+":
+                    mflCislo2 = Convert.ToSingle(txtDisplej.Text);
+                    menAktOperace = enOperace.plus;
+                    mblNovaOperace = true;
+                    break;
+                // minus
+                case "-":
+                    mflCislo2 = Convert.ToSingle(txtDisplej.Text);
+                    menAktOperace = enOperace.minus;
+                    mblNovaOperace = true;
+                    break;
+                // krat
+                case "*":
+                    mflCislo2 = Convert.ToSingle(txtDisplej.Text);
+                    menAktOperace = enOperace.krat;
+                    mblNovaOperace = true;
+                    break;
+                // deleno
+                case ":":
+                    mflCislo2 = Convert.ToSingle(txtDisplej.Text);
+                    menAktOperace = enOperace.deleno;
+                    mblNovaOperace = true;
+                    break;
+                // rovnase
+                case "=":
+                    mflCislo1 = Convert.ToSingle(txtDisplej.Text);
+
+                    if (menAktOperace == enOperace.plus) mflVysledek = mflCislo2 + mflCislo1;
+                    else if (menAktOperace == enOperace.minus) mflVysledek = mflCislo2 - mflCislo1;
+                    else if (menAktOperace == enOperace.krat) mflVysledek = mflCislo2 * mflCislo1;
+                    else if (menAktOperace == enOperace.deleno) mflVysledek = mflCislo2 / mflCislo1;
+                    else mflVysledek = mflCislo1;
+                   
+                    menAktOperace = enOperace.rovnase;
+                    mblNovaOperace = true;
+                    txtDisplej.Text = mflVysledek.ToString();
+                    break;
+            }    
+        }
+
+        private void Kalkulačka_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VseVymazat()
+        {
+           // clear
+            mblNovaOperace = true;
+
+            // init promennych
+            mflCislo1 = mflCislo2 = mflVysledek = 0;
+            menAktOperace = enOperace.clear;
+
+        }
+        private void PosledniVymazat()
+        {
+            string a;
+
+            mblNovaOperace = true;
+
+            a = txtDisplej.Text;
+            a=a.Substring(0,a.Length-1);
+            txtDisplej.Text = a;
+        }
+    
+    }   
+    
+    
+}
